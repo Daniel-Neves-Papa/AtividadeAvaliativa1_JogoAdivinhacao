b96ebfdae08d1e9cd3c781fdc22d5776e4738c55
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-04-02 13:42:13 -0300

    Formatei e mudei os textos do código

--- Jogo_Adivinhação/main.py
+++ Jogo_Adivinhação/main.py
@@ -3,14 +3,16 @@ import random
 #JOGO ADIVINHAÇÃO 
 
 #TELA INICIAL
-print("---Seja bem-vindo ao Jogo da Advinhação!!---")
+print("                ---Seja bem-vindo ao Jogo da Advinhação!!---                  ")
 
-print("Nesse jogo, você vai precisar advinhar o número secreto entre [1000 e 9999], a partir da 5a tentativa, o jogo ajudara com dicas.")
+print(" Nesse jogo, você vai precisar advinhar o número secreto entre [1000 e 9999]. ")
+print("             A partir da 5ª tentativa, o jogo ajudara com dicas.              ")
 
+print()
 
 #COMEÇAR O JOGO - LOOP WHILE
 
-jogar = int(input("Você quer jogar? (1/0) (1 = sim) (0 = não)")) 
+jogar = int(input("Você quer jogar? (1 = sim/0 = não) ")) #Pergunta se o jogador quer jogar o jogo, podemos trocar por aperte enter para jogar, ja que ele abriu o jogo
 
 while jogar == 1:
     print("Jogando")
@@ -23,32 +25,29 @@ while jogar == 1:
     
 #GERAR NÚMERO ALEATÓRIO
 
-        
-        codigoTentativa=int(input("Digite um numero entre 1000 e 9999: "))
+        print()
+        print(f"--{contTentativa}ª TENTATIVA--")
+        codigoTentativa=int(input("Digite um número de quatro digitos: "))
 
-        while 1000 > codigoTentativa or codigoTentativa > 9999: #VERIFICANDO SE O NÚMERO É VÁLIDO
-            print("Número inválido")
-            codigoTentativa=int(input("Digite um numero entre 1000 e 9999: "))
+        while 1000 > codigoTentativa or codigoTentativa > 9999: #Verificando se o número é válido, se não for, pede o input novamente.
+            print("ERRO: Você inseriu um número inválido!")
+            codigoTentativa=int(input("Digite novamente um número de quatro digitos "))
 
     #TENTATIVAS:
-    
-        print(f"Você está na tentativa: {contTentativa}")
 
         for x in range(3,-1,-1): #SEPARANDO O NÚMERO EM 4 DIGITOS
 
-            nCodigoTentativa = (codigoTentativa % (10**(x+1))) // 10**x #DIVIDE A TENTATIVA PRIMEIRO POR 10000(10**4) E PEGA O RESTO, DEPOIS DIVIDE POR 1000
+            #Divide o número por 10000 (10^4) e pega o resto, depois divide por 1000 (10^3), definindo a variável como o primeiro dígito e assim por diante.
+            nCodigoTentativa = (codigoTentativa % (10**(x+1))) // 10**x             
             nCodigoSecreto = (codigoSecreto % (10**(x+1))) // 10**x
-
-            #print(x)
-            #print(nCodigoTentativa)
-            #print(nCodigoSecreto)
             
-            if nCodigoTentativa == nCodigoSecreto:
+            if nCodigoTentativa == nCodigoSecreto: #Imprime na tela os números que o jogador acertou, EXEMPLO: 1 _ 3 _
                 print(nCodigoTentativa, end=' ')
             else:
                 print("_", end=" ")
+
         print("")
-    jogar = int(input("Você quer jogar? (1/0)"))
+    jogar = int(input("Você quer jogar novamente? (1 = sim/0 = não)"))
     
               
 #DICAS
