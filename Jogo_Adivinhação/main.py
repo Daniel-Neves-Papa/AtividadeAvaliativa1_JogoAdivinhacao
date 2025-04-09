33c117f00acdac8a93d9e57ebf2b7fe7d9eb8b5d
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-04-09 10:48:07 -0300

    Squashed commit of the following:
    commit 4f7e5eb0f6f5aec90dbe6369ed5ecde63dd8a068
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 9 10:47:51 2025 -0300
    
        Formatei e deixei mais legível
    
    commit 87e924787135bd7eb4bf22c97f8f16dac8240672
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 9 09:26:52 2025 -0300
    
        Update main.py
    
        Co-Authored-By: Daniel-Neves-Papa <200940226+Daniel-Neves-Papa@users.noreply.github.com>
        Co-Authored-By: Lucas Kendi <189281214+lkendi06@users.noreply.github.com>
    
    commit 9ff62db5f8a9d96ca23f7a5dd4fbf124533bda13
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Tue Apr 8 22:26:20 2025 -0300
    
        Sistema de dicas FEITO
    
        Lucas Kendi, João Pedro e Daniel Papa, através do Live Share
    
    commit 4045df536112af80a9d28e1d1697ce22b180040a
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Tue Apr 8 20:53:10 2025 -0300
    
        link liveshare02
    
    commit b96ebfdae08d1e9cd3c781fdc22d5776e4738c55
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 2 13:42:13 2025 -0300
    
        Formatei e mudei os textos do código
    
    commit 8838291d9e9720c5b009909a73594a480ad27282
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 2 09:28:34 2025 -0300
    
        Inicio da programação
    
    commit 03419a74e87a7ec01761135c1b5252f2cd20977a
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 2 08:23:18 2025 -0300
    
        Update README.md
    
    commit 44ffbd0cfca9ee959fb8d2adb8369cf5fa967a11
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 2 08:20:30 2025 -0300
    
        delete main.py
    
        arquivo duplicado
    
    commit 7bae1500f288b9e759355448dbfe3f750213c458
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 2 08:18:04 2025 -0300
    
        Criei main.py

---
+++ Jogo_Adivinhação/main.py
@@ -0,0 +1,162 @@
+import random
+
+#JOGO ADIVINHAÇÃO - ATIVADE AVALIATIVA 01
+
+#TELA INICIAL
+
+print("------------------------------------------------------------------------------")
+print("                   Seja bem-vindo ao Jogo da Advinhação!!                     ")
+print()
+print(" Nesse jogo, você vai precisar advinhar o número secreto entre [1000 e 9999]. ")
+print("          A partir da 5ª tentativa, o jogo vai te ajudar com dicas.           ")
+print("------------------------------------------------------------------------------")
+print()
+
+#COMEÇAR O JOGO - LOOP WHILE
+
+input("                              <Aperte ENTER>")
+
+jogar = 1
+while jogar == 1:
+    print()
+    print("O JOGO COMEÇOU")
+
+    #GERAR NÚMERO ALEATÓRIO
+
+    codigoSecreto = random.randint(1000,9999)
+    print(codigoSecreto)
+
+    #TENTATIVAS:
+
+    for contTentativa in range(1,11): #Quantidade de tentativas do usuário
+
+        print()
+        
+        print(f"--{contTentativa}ª TENTATIVA--")
+        codigoTentativa = int(input("Digite sua tentativa: "))
+
+        while 1000 > codigoTentativa or codigoTentativa > 9999: #Verificando se o número é válido, se não for, pede o input novamente.
+            print("ERRO: Você inseriu um número inválido!")
+            codigoTentativa=int(input("Digite novamente um número de quatro digitos: "))
+
+        print()
+
+        #Se o jogador acertar o código:
+
+        if codigoSecreto == codigoTentativa:
+
+            print("PARABÉNS, você acertou o código!!!")
+            print(f"O código era: {codigoSecreto}")
+            print(f"Você acertou o código em: {contTentativa} tentativas")
+            print()
+            break
+
+        for x in range(3,-1,-1): #SEPARANDO O NÚMERO EM 4 DIGITOS
+
+            #Divide o número por 10000 (10^4) e pega o resto, depois divide por 1000 (10^3), definindo a variável como o primeiro dígito e assim por diante.
+
+            nCodigoTentativa = (codigoTentativa % (10**(x+1))) // 10**x             
+            nCodigoSecreto = (codigoSecreto % (10**(x+1))) // 10**x
+
+            #EX: tentativa = 1234, x = 3, primeiro digito: numero % 10000 // 1000 -> 1234 % 10000 = 1234 -> 1234 // 1000 = 1
+
+            if nCodigoTentativa == nCodigoSecreto: #Imprime na tela os números que o jogador acertou, EXEMPLO: 1 _ 3 _
+                print(nCodigoTentativa, end=' ')
+            else:
+                print("_", end=" ")
+
+        print()
+        print()
+        
+        if contTentativa == 10 and codigoTentativa != codigoSecreto:
+            print("Número de tentativas excedidas :( ") 
+            print("VOCÊ NÃO CONSEGEGIU ACERTAR.")
+            print(f"O código era: {codigoSecreto}")
+            break
+            
+        #DICAS:
+
+        if contTentativa >= 5:
+            print("Aqui vai uma dica:", end=" ")
+            dicaEnviada = 0
+
+            while dicaEnviada == 0:
+
+                posicaoDica = random.randint(1,4) # _ _ _ _
+
+                #Define os digitos na (posicaoDica)ª posição da tentativa. EX: se a posição da dica == 3, ele vai achar o terceiro digito.
+
+                nCodigoTentativa = (codigoTentativa % (10**(posicaoDica))) // 10**(posicaoDica-1) 
+                nCodigoSecreto = (codigoSecreto % (10**(posicaoDica))) // 10**(posicaoDica-1)
+                
+                #n1 = codigoTentativa % 10000) // 1000. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+                #n1 = codigoTentativa % 1000) // 100. EX: 1234 => n1 = 1234 % 1000 = 234 => 234 // 100 = 2
+                #n1 = codigoTentativa % 100) // 10. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 3
+                #n1 = codigoTentativa % 10) // 1. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 4
+
+                #s1 = codigoTentativa % 10000) // 1000. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+                #s1 = codigoTentativa % 1000) // 100. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+                #s1 = codigoTentativa % 100) // 10. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+                #s1 = codigoTentativa % 10) // 1. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+
+                #if n1 == s1
+                #if n2 == s2
+                #if n1 == s3
+                #if n2 == s4
+                
+
+                #if ncodigo tentativa == ncodigosecreto:
+
+                #EX: tentativa = 1234, posicaoDica = 1, ultimo digito: numero % 10**1 // 10 ** 0 -> 1234 % 10 = 4 -> 4 // 1 = 4
+
+                if nCodigoTentativa != nCodigoSecreto:
+
+                    tipoDica = random.randint(1,2) #Par/Impar ou <5/>=5
+                    dicaEnviada = 1
+
+                    if tipoDica == 1:
+
+                        if nCodigoSecreto % 2 == 0:
+                            print(f"O {5-posicaoDica}° número é Par")
+                            dica = "PAR"
+                        else:
+                            print(f"O {5-posicaoDica}° número é Ímpar")
+                            dica = "ÍMPAR"
+
+                    elif tipoDica == 2:
+
+                        if nCodigoSecreto >= 5:
+                            print(f"O {5-posicaoDica}° número é maior ou igual a 5")
+                            dica = "(>=5)"
+                        else:
+                            print(f"O {5-posicaoDica}° número é menor que 5")
+                            dica = "(<5)"
+                    print()
+                    for x in range(3,-1,-1):
+
+                        nCodigoTentativa = (codigoTentativa % (10**(x+1))) // 10**x             
+                        nCodigoSecreto = (codigoSecreto % (10**(x+1))) // 10**x
+
+                        if nCodigoTentativa == nCodigoSecreto:
+                            print(nCodigoTentativa, end=" ")
+                        elif x + 1 == posicaoDica:
+                            print(dica, end=" ")
+                        else:
+                            print("_", end=" ")
+                    print()
+                    print()
+                    input("<Aperte ENTER>")
+
+
+    print("Você quer jogar novamente? (1 = sim/0 = não) ")
+    jogar = int(input(">")) #Jogar novamente:
+    
+    while not(jogar == 1) and not(jogar == 0):
+        print()
+        print("ERRO: Você inseriu um número inválido!")
+        print("Você quer jogar novamente? (1 = sim/0 = não)")
+        jogar = int(input(">"))
+    if jogar == 0:
+        print()
+        print("Obrigado por jogar!!!")
+        break
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AtividadeAvaliativa1_JogoAdivinhacao
 Atividade avaliativa da matéria Algoritmos de Programação
+Professora: Lucia
+GRUPO : DANIEL, JOÃO PEDRO e LUCAS KENDI
+
+link - liveshare dia 1: https://prod.liveshare.vsengsaas.visualstudio.com/join?FC896ADC4FD0D5459D40A4D8B3F81B8892A3
