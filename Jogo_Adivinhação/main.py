87e924787135bd7eb4bf22c97f8f16dac8240672
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-04-09 09:26:52 -0300

    Update main.py
    Co-Authored-By: Daniel-Neves-Papa <200940226+Daniel-Neves-Papa@users.noreply.github.com>
    Co-Authored-By: Lucas Kendi <189281214+lkendi06@users.noreply.github.com>

--- Jogo_Adivinhação/main.py
+++ Jogo_Adivinhação/main.py
@@ -4,20 +4,22 @@ import random
 
 #TELA INICIAL
 
-print("                ---Seja bem-vindo ao Jogo da Advinhação!!---                  ")
+print("------------------------------------------------------------------------------")
+print("                   Seja bem-vindo ao Jogo da Advinhação!!                     ")
 print()
 print(" Nesse jogo, você vai precisar advinhar o número secreto entre [1000 e 9999]. ")
-print("             A partir da 5ª tentativa, o jogo ajudara com dicas.              ")
-
+print("          A partir da 5ª tentativa, o jogo vai te ajudar com dicas.           ")
+print("------------------------------------------------------------------------------")
 print()
 
 #COMEÇAR O JOGO - LOOP WHILE
 
-print("Você quer jogar? (1 = sim/0 = não) ")
-jogar = int(input(">")) #Pergunta se o jogador quer jogar o jogo, podemos trocar por aperte enter para jogar, ja que ele abriu o jogo
+input("                             <Aperte Enter>")
 
+jogar = 1
 while jogar == 1:
-    print("Jogando")
+    print()
+    print("O JOGO COMEÇOU")
 
     #GERAR NÚMERO ALEATÓRIO
 
@@ -33,7 +35,7 @@ while jogar == 1:
         print(f"--{contTentativa}ª TENTATIVA--")
         codigoTentativa = int(input("Digite sua tentativa: "))
 
-        while 1000 > codigoTentativa or codigoTentativa > 9999: #Verificando se o número é válido, se não for, pede o input novamente.
+        while 1000 > codigoTentativa or codigoTentatillllva > 9999: #Verificando se o número é válido, se não for, pede o input novamente.
             print("ERRO: Você inseriu um número inválido!")
             codigoTentativa=int(input("Digite novamente um número de quatro digitos: "))
 
@@ -65,18 +67,20 @@ while jogar == 1:
 
         print()
         print()
+        
         if contTentativa == 10 and codigoTentativa != codigoSecreto:
             print("Número de tentativas excedidas :( ") 
             print("VOCÊ NÃO CONSEGEGIU ACERTAR.")
             print(f"O código era: {codigoSecreto}")
+            break
             
         #DICAS:
 
         if contTentativa >= 5:
 
-            dica = False
+            dicaEnviada = 0
 
-            while dica == False:
+            while dicaEnviada == 0:
 
                 posicaoDica = random.randint(1,4) # _ _ _ _
 
@@ -84,26 +88,56 @@ while jogar == 1:
 
                 nCodigoTentativa = (codigoTentativa % (10**(posicaoDica))) // 10**(posicaoDica-1) 
                 nCodigoSecreto = (codigoSecreto % (10**(posicaoDica))) // 10**(posicaoDica-1)
+                
+                #n1 = codigoTentativa % 10000) // 1000. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+                #n1 = codigoTentativa % 1000) // 100. EX: 1234 => n1 = 1234 % 1000 = 234 => 234 // 100 = 2
+                #n1 = codigoTentativa % 100) // 10. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 3
+                #n1 = codigoTentativa % 10) // 1. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 4
+
+                #s1 = codigoTentativa % 10000) // 1000. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+                #s1 = codigoTentativa % 1000) // 100. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+                #s1 = codigoTentativa % 100) // 10. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+                #s1 = codigoTentativa % 10) // 1. EX: 1234 => n1 = 1234 % 10000 = 1234 => 1234 // 1000 = 1
+
+                #if n1 == s1
+                #if n2 == s2
+                #if n1 == s3
+                #if n2 == s4
+                
+
+                #if ncodigo tentativa == ncodigosecreto:
 
                 #EX: tentativa = 1234, posicaoDica = 1, ultimo digito: numero % 10**1 // 10 ** 0 -> 1234 % 10 = 4 -> 4 // 1 = 4
 
                 if nCodigoTentativa != nCodigoSecreto:
 
                     tipoDica = random.randint(1,2) #Par/Impar ou <5/>=5
+                    dicaEnviada = 1
+
                     if tipoDica == 1:
+
                         if nCodigoSecreto % 2 == 0:
                             print(f"O {5-posicaoDica}° número é Par")
-                            dica = True
                         else:
                             print(f"O {5-posicaoDica}° número é Ímpar")
-                            dica = True
+
                     elif tipoDica == 2:
+
                         if nCodigoSecreto >= 5:
                             print(f"O {5-posicaoDica}° número é maior ou igual a 5")
-                            dica = True
                         else:
                             print(f"O {5-posicaoDica}° número é menor que 5")
-                            dica = True
 
-    print("Você quer jogar? (1 = sim/0 = não) ")
+
+    print("Você quer jogar novamente? (1 = sim/0 = não) ")
     jogar = int(input(">")) #Jogar novamente:
+    
+    while not(jogar == 1) and not(jogar == 0):
+        print()
+        print("ERRO: Você inseriu um número inválido!")
+        print("Você quer jogar novamente? (1 = sim/0 = não)")
+        jogar = int(input(">"))
+    if jogar == 0:
+        print()
+        print("Obrigado por jogar!!!")
+        break
